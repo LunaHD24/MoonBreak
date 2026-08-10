811cde9b6bf1322f241d408b56ba480b75e3ae91
LunaHD24 <yannick.rumelfanger@gmail.com>
2026-08-10 11:34:48 +0200

    Removed testing code

--- src/main/java/dev/lunaa/moonbreak/MoonBreak.java
+++ src/main/java/dev/lunaa/moonbreak/MoonBreak.java
@@ -2,16 +2,10 @@ package dev.lunaa.moonbreak;
 
 import dev.lunaa.moonbreak.block.CustomBlockLoader;
 import dev.lunaa.moonbreak.block.CustomBlockManager;
-import dev.lunaa.moonbreak.block.CustomBlockType;
 import dev.lunaa.moonbreak.listener.*;
-import dev.lunaa.moonbreak.registry.BuiltinRegistries;
 import dev.lunaa.moonbreak.registry.Registrable;
 import dev.lunaa.moonbreak.registry.ResourceRegistryImpl;
-import dev.lunaa.moonbreak.tool.CustomToolType;
-import net.kyori.adventure.key.Key;
-import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
@@ -19,7 +13,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.logging.Logger;
@@ -36,9 +29,6 @@ public final class MoonBreak extends JavaPlugin {
     private @MonotonicNonNull CustomBlockLoader blockLoader;
     private @MonotonicNonNull BreakingService breakingService;
 
-    public static CustomToolType HAMMER;
-    public static CustomBlockType STONE_OBSIDIAN;
-
     @Override
     public void onEnable() {
         instance = this;
@@ -47,20 +37,6 @@ public final class MoonBreak extends JavaPlugin {
         initializeFields();
         initializeServices();
         initializeApi();
-
-        HAMMER = CustomToolType.builder()
-                .material(Material.WOODEN_PICKAXE)
-                .name(Component.text("HAMMER"))
-                .lore(List.of(Component.text("Lore 1"), Component.text("Lore 2"), Component.text("Lore 3")))
-                .speed(CustomToolType.MiningSpeed.GOLD)
-                .affectedUnderwater(false)
-                .miningLevel(CustomToolType.MiningLevel.DIAMOND)
-                .build();
-
-        STONE_OBSIDIAN = CustomBlockType.of(Material.OBSIDIAN, Material.STONE);
-
-        BuiltinRegistries.TOOL_TYPE.register(Key.key(this, "hammer"), HAMMER);
-        BuiltinRegistries.BLOCK_TYPE.register(Key.key(this, "stone_obsidian"), STONE_OBSIDIAN);
     }
 
     @Override
